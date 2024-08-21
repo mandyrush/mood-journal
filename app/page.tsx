c58f7b23391d0ad44a23bc23fcaa934fb65ec609
mandyrush <mandyrush85@gmail.com>
2024-08-21 15:20:33 -0600

    add initial pages and model schemas

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Mood Journal</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          Track your mood with journal entries
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
